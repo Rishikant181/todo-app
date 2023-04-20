e13c26683c999cfe3fb6c7010c60d6cfeff32203
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-20 22:50:23 +0530

    Added a single task item

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 /**
  * The different todo tasks
  */
-const tasks = [];
+const tasks = ['hello', 'world'];
+
+/**
+ * Creates a new Task item element.
+ *
+ * @param {*} task The task to store.
+ * @returns The new TaskItem.
+ */
+const TaskItem = (task) => {
+	// Creating a new list item to store the task
+	const element = document.createElement('li');
+
+	// Adding styling
+	element.classList = ['list-group-item'];
+
+	// Inserting the task into the task item
+	element.innerHTML = task;
+
+	return element;
+};
