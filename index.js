4debf32a7a785bea5b5bdfc3b492de488e149cf4
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-22 11:26:34 +0530

    completed basic todo app

--- index.js
+++ index.js
@@ -4,20 +4,50 @@
 const tasks = ['hello', 'world'];
 
 /**
- * Creates a new Task item element.
- *
- * @param {*} task The task to store.
- * @returns The new TaskItem.
+ * The total number of tasks present
  */
-const TaskItem = (task) => {
-	// Creating a new list item to store the task
-	const element = document.createElement('li');
+let taskCount = 0;
 
-	// Adding styling
-	element.classList = ['list-group-item'];
+/**
+ * Adds a new task to the tasklist.
+ *
+ * @param {*} task The task to be added.
+ */
+function addTask(task) {
+	// Incrementing task count
+	taskCount++;
 
-	// Inserting the task into the task item
-	element.innerHTML = task;
+	// Creating a new task item
+	const taskItem = `
+	<li class="list-group-item task-item" id="task-id-${taskCount}">
+		<div class="row">
+			<!-- DONE CHECKBOX -->
+			<div class="col-1 p-3">
+				<input class="form-check-input task-control" type="checkbox" value=""
+					id="task-check-${taskCount}" onclick="taskDone(this)"/>
+			</div>
+			<!---->
 
-	return element;
-};
+			<!-- TASK CONTENT -->
+			<div class="col-11 m-auto text-start">
+				<h3 class="task-content">${task}</h3>
+			</div>
+			<!---->
+		</div>
+	</li>`;
+
+	document.getElementById('task-list').innerHTML += taskItem;
+}
+
+/**
+ * Remove the target task from the list of tasks.
+ *
+ * @param {*} target The task to be removed.
+ */
+function taskDone(target) {
+	// Getting the task id
+	const taskId = (target.id + '').substring((target.id + '').lastIndexOf('-') + 1);
+
+	// Removing the task item with the given id
+	document.getElementById(`task-id-${taskId}`).remove();
+}
